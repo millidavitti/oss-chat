7bb443815b133dc23d480b68085a4a8f4da2dfed
davitti <abuadonald@gmail.com>
2025-06-13 03:23:38 +0100

    refactor(config): format allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	allowedDevOrigins: [
+		"local-origin.dev",
+		"*.local-origin.dev",
+		"192.168.81.189",
+	],
 };
 
 export default nextConfig;
